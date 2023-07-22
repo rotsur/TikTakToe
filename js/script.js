870f857d9c93ee9ee005864b35f4a4b7514e3047
Raman Tsurkan <romanred99@gmail.com>
2023-07-22 18:58:20 +0200

    add win

--- js/script.js
+++ js/script.js
@@ -7,7 +7,37 @@ function start(cells) {
         cell.addEventListener('click', function step() {
             this.textContent = ['X', 'O'][i % 2];
             this.removeEventListener('click', step);
+
+            if (isVictory(cells)) {
+                alert(this.textContent);
+            }
+
             i++;
         });
     }
+}
+
+function isVictory(cells) {
+    let combs = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let comb of combs) {
+        if (
+            cells[comb[0]].textContent == cells[comb[1]].textContent &&
+            cells[comb[1]].textContent == cells[comb[2]].textContent &&
+            cells[comb[0]].textContent != ''
+        ) {
+            return true;
+        }
+    }
+
+    return false;
 }
