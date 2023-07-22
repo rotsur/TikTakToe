597cac320a58ce60d99e108fab854de9a3f229ad
Raman Tsurkan <romanred99@gmail.com>
2023-07-22 18:45:09 +0200

    add X and O

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 let cells = document.querySelectorAll('#field td');
+start(cells);
+function start(cells) {
+    let i = 0;
+    for (let cell of cells) {
+        cell.addEventListener('click', function step() {
+            this.textContent = ['X', 'O'][i % 2];
+            this.removeEventListener('click', step);
+            i++;
+        });
+    }
+}
